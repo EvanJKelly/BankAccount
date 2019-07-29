639972bb0a8c9e104191f01c5c6c49835f7f9d1a
evanjkelly123@gmail.com <evanjkelly123@gmail.com>
2019-07-29 14:55:35 -0700

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a customers bank account
+    /// </summary>
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        
+        public string Owner { get; set; }
+        
+        public double Balance { get; private set; }
+
+
+        /// <summary>
+        /// Adds an amoutn to the current balacne
+        /// and reutns the updated balance
+        /// </summary>
+        /// <param name="amt">The amount to add</param>
+        /// <returns></returns>
+        public double Deposit(double amt)
+        {
+            if(amt < 0)
+            {
+                throw new ArgumentException($"{nameof(amt)}amt cannot be negative");
+            }
+
+            Balance += amt;
+            return Balance;
+        }
+        
+
+        public double Withdraw(double amt)
+        {
+            Balance -= amt;
+            return Balance;
+        }
+
+    }
+}
---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void Deposit_PoisitveAmount_AddsToBalance()
+        {
+            // AAA Pattern (Arrange, Act, Assert)
+
+            //Arrange - Init objects/variables
+            Account checking = new Account();
+            double depositAmt = 10;
+            double expectedBalance = 10;
+
+            //Act - Call method under test
+            checking.Deposit(depositAmt);
+
+            //Assert - Verification step
+            Assert.AreEqual(expectedBalance, checking.Balance);
+
+        }
+
+        [TestMethod]
+        public void Deposit_PositiveAmount_ReturnsUpdatedBalance()
+        {
+            //Arrange
+            Account acc = new Account();
+            double depositAmt = 10.55;
+            double expectedReturn = 10.55;
+
+            //Act
+            double result = acc.Deposit(depositAmt);
+
+            //Assert
+            Assert.AreEqual(expectedReturn, result);
+        }
+
+        [TestMethod]
+        public void Depost_NegativeAmount_ThrowsArgumentException()
+        {
+            //Arrange
+            Account acc = new Account();
+            double depositAmt = -1;
+
+            //Act => Assert 
+            Assert.ThrowsException<ArgumentException>
+                (() => acc.Deposit(depositAmt));
+        }
+    }
+}
